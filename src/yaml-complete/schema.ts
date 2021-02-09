0f4ba4e1af376c2c96f338aaad8530c6063d34fc
Ian Sinnott <ian@iansinnott.com>
2021-02-09 10:16:14 +0800

    Add a note about the fork in the source code

--- src/yaml-complete/schema.ts
+++ src/yaml-complete/schema.ts
@@ -1,50 +1,57 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-export const TRAEFIK_YAML_SCHEMA = 'traefikode';
+export const TRAEFIK_YAML_SCHEMA = "traefikode";
+
+/*
+@note This file is the reason I created the fork. 
+It's unclear to me whether you need the redhat extension or not. The readme says
+it's incompatible, and the source code itself says it's required... however I
+don't use that plugin and the completions work fine without it.
+*/
 
 export const getYamlApi = async () => {
-	const ext = vscode.extensions.getExtension('redhat.vscode-yaml');
-	const currentFile = vscode.window.activeTextEditor;
-	if (
-		ext &&
-		//@ts-ignore
-		new RegExp(/compose\.ya?ml$/).test(currentFile.document.uri.fsPath)
-	) {
-		vscode.window.showWarningMessage(
-			'Traefik Intellisense will not work while the Red Hat yaml extension is enabled.'
-		);
-	}
+  const ext = vscode.extensions.getExtension("redhat.vscode-yaml");
+  const currentFile = vscode.window.activeTextEditor;
+  if (
+    ext &&
+    //@ts-ignore
+    new RegExp(/compose\.ya?ml$/).test(currentFile.document.uri.fsPath)
+  ) {
+    vscode.window.showWarningMessage(
+      "Traefik Intellisense will not work while the Red Hat yaml extension is enabled.",
+    );
+  }
 
-	if (!ext) {
-		vscode.window.showWarningMessage(
-			"You must have 'YAML Support by Red Hat' installed in order to use this extension. Enabling the yaml extension will disable intellisense in docker-compose.yaml files. The yaml linting only works in traefik.yaml files."
-		);
-		return undefined;
-	}
+  if (!ext) {
+    vscode.window.showWarningMessage(
+      "You must have 'YAML Support by Red Hat' installed in order to use this extension. Enabling the yaml extension will disable intellisense in docker-compose.yaml files. The yaml linting only works in traefik.yaml files.",
+    );
+    return undefined;
+  }
 
-	const yamlPlugin = await ext.activate();
-	if (!yamlPlugin || !yamlPlugin.registerContributor) {
-		vscode.window.showWarningMessage(
-			"Please upgrade 'YAML Support by Red Hat' via the Extensions pane."
-		);
-		return undefined;
-	}
-	return yamlPlugin;
+  const yamlPlugin = await ext.activate();
+  if (!yamlPlugin || !yamlPlugin.registerContributor) {
+    vscode.window.showWarningMessage(
+      "Please upgrade 'YAML Support by Red Hat' via the Extensions pane.",
+    );
+    return undefined;
+  }
+  return yamlPlugin;
 };
 
 export function onRequestSchemaURI(resource: string): string | undefined {
-	if (resource.endsWith('/traefik.yaml') || resource.endsWith('/traefik.yml')) {
-		return `${TRAEFIK_YAML_SCHEMA}://schema/traefik`;
-	}
-	return undefined;
+  if (resource.endsWith("/traefik.yaml") || resource.endsWith("/traefik.yml")) {
+    return `${TRAEFIK_YAML_SCHEMA}://schema/traefik`;
+  }
+  return undefined;
 }
 
-let responseCache = '';
+let responseCache = "";
 export async function getSchemaContent() {
-	if (responseCache !== '') {
-		return responseCache;
-	}
-	const response = JSON.stringify(require('../../schemas/traefik-all.json'));
-	responseCache = response;
-	return responseCache;
+  if (responseCache !== "") {
+    return responseCache;
+  }
+  const response = JSON.stringify(require("../../schemas/traefik-all.json"));
+  responseCache = response;
+  return responseCache;
 }
